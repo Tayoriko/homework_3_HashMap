9b8a24fcd94977475ece6a0038fad29a59a9b954
Iurii Razumovskii <sintronix@gmail.com>
2024-01-10 11:44:33 +0200

    Homework for lesson 5.5: - fix method addStudent for check null values - fix method addStudent for public modifier - fix method top3 for check null elements in tree and work with less then 3 records

--- src/main/java/students_easy/ResultBoard.java
+++ src/main/java/students_easy/ResultBoard.java
@@ -11,22 +11,29 @@ public class ResultBoard {
 
     TreeMap<Float, String> board = new TreeMap<>();
 
-    public void ResultsBoard(){
+    public void ResultsBoard() {
 
     }
 
-    void addStudent(String name, Float score) {
-        this.board.put(score, name);
+    public void addStudent(String name, Float score) {
+        if (name != null && score != null) {
+            this.board.put(score, name);
+        } else throw new RuntimeException("Incorrect input data");
     }
 
-    public List<String> top3 () {
+    public List<String> top3() {
         List<String> result = new ArrayList<>();
-        Float key = this.board.lastKey();
-        result.add(this.board.get(key));
-        key = this.board.lowerKey(key);
-        result.add(this.board.get(key));
-        key = this.board.lowerKey(key);
-        result.add(this.board.get(key));
+        Float key = 0.0F;
+        if (board.lastKey() != null) {
+            key = this.board.lastKey();
+        }
+        for (int i = 0; i < 2; i++) {
+            result.add(this.board.get(key));
+            if (board.lowerKey(key) != null) {
+                key = this.board.lowerKey(key);
+            } else break;
+        }
         return result;
     }
 }
+
--- src/test/java/students_easy/ResultBoardTest.java
+++ src/test/java/students_easy/ResultBoardTest.java
@@ -2,7 +2,6 @@ package students_easy;
 
 import org.junit.jupiter.api.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 class ResultBoardTest {
@@ -13,22 +12,22 @@ class ResultBoardTest {
     void beforeAll(){
         board.addStudent("Oleg", 3.13F);
         board.addStudent("Olga", 1.03F);
-        board.addStudent("Onni", 2.1F);
-        board.addStudent("Oppa", 4.7F);
-        board.addStudent("Oppay", 4.0F);
     }
 
     @DisplayName("Add 5 score to board")
     @Test
     void addStudent() {
+        board.addStudent("Onni", 2.1F);
+        board.addStudent("Oppa", 4.7F);
+        board.addStudent("Oppay", 4.0F);
+        //board.addStudent(null, 4.0F);
         Assertions.assertEquals(5, board.getBoard().size());
     }
 
     @DisplayName("Get 3 highest score from board")
     @Test
     void top3() {
-        List<String> top3Scores = new ArrayList<>();
-        top3Scores = board.top3();
-        Assertions.assertEquals("Oppay", top3Scores.get(1));
+        List<String> top3Scores = board.top3();
+        Assertions.assertEquals("Olga", top3Scores.get(1));
     }
 }
